3adc5481305b2f381fe506cc2d412c1dc00bbf97
2ooom <dmitry.parfenchik@gmail.com>
2013-11-18 13:20:36 +0300

    missing file added

---
+++ SimpleFTP.Core/FileSystem/FileSystemItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFTP.Core.FileSystem
+{
+    public class FileSystemItem
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public string Size { get; set; }
+        public bool IsDirectory { get; set; }
+        public bool IsParentNavigation { get; set; }
+        public bool IsDrive { get; set; }
+    }
+}
